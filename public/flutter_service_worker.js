2062f526784019f44084bec78e5c48b8d8849cc1
Aniket sindhu <sindhuaniket@gmail.com>
2021-04-29 21:09:47 +0530

    hosting setup done

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d61acac7422f136a7d19b5e666350c9d",
-"index.html": "f492ef3198bc6fc3d2dd01597464edb1",
-"/": "f492ef3198bc6fc3d2dd01597464edb1",
-"main.dart.js": "6ba5984cd9cf14c06e17d05ee9d38ee5",
+"index.html": "54ec71b6b36009209135f8bad865cf15",
+"/": "54ec71b6b36009209135f8bad865cf15",
+"main.dart.js": "8c8866fa42fbe6433156ab3030b1fe0a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
